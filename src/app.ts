502a979f9a735077be1905f7c6930d1b33226ba5
Fabian <fabianwaszkiewicz09@gmail.com>
2023-03-04 11:10:57 +0100

    render project items with a class

--- src/app.ts
+++ src/app.ts
@@ -141,6 +141,35 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void;
 }
 
+// ProjectItem Class
+class ProjectItem extends Component<HTMLUListElement, HTMLLinkElement> {
+  private project: Project;
+
+  get persons() {
+    if(this.project.people === 1)
+      return '1 Person';
+
+    return `${this.project.people} Persons`;
+
+  }
+
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure() {}
+
+  renderContent() {
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent = `${this.persons} assigned`;
+    this.element.querySelector('p')!.textContent = this.project.description;
+  }
+}
+
 // ProjectList Class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[];
@@ -160,11 +189,10 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
     listEl.innerHTML = '';
 
-    for(const prjItem of this.assignedProjects) {
-      const listItem = document.createElement('li');
-      listItem.textContent = prjItem.title;
-      listEl.appendChild(listItem);
-    }
+    for(const prjItem of this.assignedProjects)
+      // eslint-disable-next-line no-new
+      new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
+
   }
 
   configure() {
